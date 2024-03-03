b4bd5c4f99a9fb56d372b34116b9ddb748351956
prince-ao <prince.addo@cix.csi.cuny.edu>
2024-03-03 09:54:05 -0500

    started frame sending process

--- backend/app/static/js/stream.js
+++ backend/app/static/js/stream.js
@@ -5,14 +5,27 @@
   const streaming = false
 
   let video = null;
+  let canvas = null;
+  let context = null
 
   async function startup() {
     video = document.getElementById("video");
+    canvas = document.getElementById("canvas")
+    context = canvas.getContext('2d');
 
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
       video.srcObject = stream;
       video.play();
+
+      setInterval(() => {
+        context.drawImage(video, 0, 0, canvas.width, canvas.height)
+        const frame = canvas.toDataURL('image/jpeg')
+
+        sendFrameToBackend()
+      }, 100)
+
+
     } catch (err) {
       console.log(`An error occurred: ${err}`)
     }
@@ -30,6 +43,9 @@
       },
       false
     )
+  }
+
+  async function sendFrameToBackend() {
   }
 
 
