5495932a526912de1f389770198b6d3372570080
prince-ao <prince.addo@cix.csi.cuny.edu>
2024-03-12 08:44:08 -0400

    login page added

--- backend/app/main/__init__.py
+++ backend/app/main/__init__.py
@@ -3,6 +3,16 @@ from flask import Blueprint, render_template
 main_bp = Blueprint('main', __name__)
 
 
-@main_bp.route('/')
+@main_bp.get('/')
 def index():
     return render_template("index.html")
+
+
+@main_bp.get('/login')
+def login():
+    return render_template("login.html")
+
+
+@main_bp.get('/present')
+def present():
+    return render_template("present.html")
