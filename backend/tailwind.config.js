6bad28c298369cc472256dac79d1d7e19385b71a
prince-ao <prince.addo@cix.csi.cuny.edu>
2024-05-08 18:46:25 -0400

    present fix

--- backend/tailwind.config.js
+++ backend/tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["./app/templates/**/*.{html,j2}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "text-gradient": "text-gradient 2.5s linear infinite",
+      },
+      keyframes: {
+        "text-gradient": {
+          "0%": { backgroundPosition: "0% center" },
+          "100%": { backgroundPosition: "200% center" },
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
